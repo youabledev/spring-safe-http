139481b20755c6c7b00dfd6a286804af2b119773
youable <youable.framios@gmail.com>
2024-12-14 15:13:31 +0900

    fix: aop error

--- src/main/java/com/youable/safehttp/config/AutoConfig.java
+++ src/main/java/com/youable/safehttp/config/AutoConfig.java
@@ -2,8 +2,10 @@ package com.youable.safehttp.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan("com.youable.safehttp")
 public class AutoConfig {
 }
