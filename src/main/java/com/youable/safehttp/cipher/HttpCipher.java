f784f8c99805a7495fd792d05c52ab3b52024ea5
youable <youable.framios@gmail.com>
2024-12-19 22:33:05 +0900

    feat: HttpCipher implements

--- src/main/java/com/youable/safehttp/cipher/HttpCipher.java
+++ src/main/java/com/youable/safehttp/cipher/HttpCipher.java
@@ -3,6 +3,16 @@ package com.youable.safehttp.cipher;
 /**
  * <p>use the httpCipher interface to implement the encryption/decryption algorithm.
  * The object you implement must be registered as a bean.
+ * <pre>{@code
+ *  @Configuration
+ *  public class CipherConfig {
+ *      @Bean
+ *      public HttpCipher aesCipher() {
+ *          return new AESCipher.AESCipherBuilder().setPRIVATE_KEY("3mlng8uGI+k2CGVDKR/EAwI+uN5pzoITSkREjzxZc7M=").build();
+ *      }
+ *  }
+ * }</pre>
+ * <p>Create Configuration to enroll HttpCipher</p>
  * @author youabledev
  * @since 0.0.1
  * @see com.youable.safehttp.advice.DecryptRequestBodyAdvice
